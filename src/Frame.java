42089888eec93e11a4c76dffbaebaeac2fffa41b
Add Gaming <88384176+AddGaming@users.noreply.github.com>
2024-06-12 17:58:35 +0200

    drawing

--- src/Frame.java
+++ src/Frame.java
@@ -1,12 +1,17 @@
 package src;
 import java.text.MessageFormat;
 import java.lang.UnsupportedOperationException;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.*;
 
 class Frame {
     final int width;
     final int height;
     final char[] chars;
+
+    static private char space(Object ignore) {
+        return (char) ' ';
+    }
 
     Frame(int width, int height, char[] chars) {
         assert width >= 0 : 
@@ -18,5 +23,19 @@ class Frame {
         this.width = width;
         this.height = height;
         this.chars = chars;
+    }
+
+    static Frame empty(int width, int height) {
+        assert width >= 0 : 
+        MessageFormat.format("Frame width ({0}) needs to be positive", width);
+        assert height >= 0 : 
+        MessageFormat.format("Frame height ({0}) needs to be positive", height);
+        final char[] chars = new char[width*height];
+        Arrays.fill(chars, ' ');
+        return new Frame(width, height, chars);
+    }
+    
+    static Frame empty(Pair<Integer, Integer> pair) {
+        return Frame.empty(pair.first, pair.second);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,18 @@ public class Main {
         // Frame b = new Frame(-1, 0, new char[]{});
         // Frame c = new Frame(0, -1, new char[]{});
         // Frame d = new Frame(1, 1, new char[]{});
-        var x = Window.dimensions().get().first;
-        System.out.println(x);
-        System.out.println("Hello World!");
+        var x = Window.dimensions().get();
+        var main_window = new Window(Frame.empty(x), Frame.empty(x));
+        // TODO: draw function should diff the frames and 
+        // composite length output for console
+        // (manual / batch cursor movement)
+        while (true) {
+            main_window.clear();
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+
+            }
+        }
     }
 }
