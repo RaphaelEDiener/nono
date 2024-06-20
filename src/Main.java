c5d56979dc18194d627cca5ea67939ff1307bf1b
Add Gaming <88384176+AddGaming@users.noreply.github.com>
2024-06-20 17:41:16 +0200

    deleted old code

--- src/Main.java
+++ src/Main.java
@@ -12,46 +12,5 @@ public class Main {
         System.out.println("port: " + main_server.port);
         main_server.start();
     }
-
-    public static void old_main(String[] args)
-    {
-        // Get windows size
-        // unix systems: 
-        // "\x1b[H"             // home
-        // "\x1b[5000;5000H"    // move to col 5000 row 5000
-        // "\x1b[6n"            // request cursor position
-        // "\x1b[H"             // home
-
-        Optional<Pair<Integer, Integer>> dim_op =
-                Window.dimensions(OSys.from_args(args));
-        if (dim_op.isEmpty()) {
-            return;
-        }
-        System.out.println(dim_op.get().first + " " + dim_op.get().second);
-        var frame = Frame.full(dim_op.get());
-        var cursor = new Cursor(0, 0, "█", frame);
-        PrintWriter out = new PrintWriter(
-                new OutputStreamWriter(System.out, StandardCharsets.UTF_16)
-        );
-        Window.test(frame, out);
-        while (true) {
-            try {
-                var key_op = Keys.from_console_in(System.in.read());
-                if (key_op.isEmpty()) {
-                    continue;
-                }
-                else {
-                    var new_ = Window.update(frame, cursor, key_op.get());
-                    frame = new_.first;
-                    cursor = new_.second;
-                    Window.clear();
-                    Window.flush(frame, cursor, out);
-                }
-            }
-            catch (IOException ignore) {
-                // Exit program
-            }
-        }
-    }
 }
 
